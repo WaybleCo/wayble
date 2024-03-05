6b50485cbe1b92dc7bf35c1ded88cfc594b676a4
aarnavbaxi <aarnavbaxi07@gmail.com>
2024-03-05 16:57:23 -0500

    center logo and text on sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,13 +22,13 @@ const Navbar = () => {
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-        <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-blue-250 bg-[#ffffff] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-          <img src={Picture} alt="wayble logo" className='h-16'/>
-            <li className='p-4 border-b border-blue-250'>Home</li>
-            <li className='p-4 border-b border-blue-250'>Hiring</li>
-            <li className='p-4 border-b border-blue-250'>About</li>
-            <li className='p-4 border-b border-blue-250'>Login</li>
-            <li className='p-4'>Sign Up</li>
+        <ul className={nav ? 'fixed left-0 top-0 w-[40%] h-full border-r border-r-blue-250 bg-[#ffffff] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+          <img src={Picture} alt="wayble logo" className='h-16 mx-auto mt-4'/>
+          <li className='p-4 border-b border-blue-250 text-center'>Home</li>
+          <li className='p-4 border-b border-blue-250 text-center'>Hiring</li>
+          <li className='p-4 border-b border-blue-250 text-center'>About</li>
+          <li className='p-4 border-b border-blue-250 text-center'>Login</li>
+          <li className='p-4 text-center'>Sign Up</li>
         </ul>
     </div>
   );
